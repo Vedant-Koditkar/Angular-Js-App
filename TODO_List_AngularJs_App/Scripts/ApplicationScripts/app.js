f8da298dd43c3211db3e7f877061b6bd754023b8
Vedant Koditkar <vedkoditkar@gmail.com>
2016-06-30 11:00:39 +0530

    Updated the delete completed tasks logic

---
+++ TODO_List_AngularJs_App/Scripts/ApplicationScripts/app.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="../angular.min.js" />
+/// <reference path="../jquery-1.9.1.min.js" />
+var app = angular.module("TodoApp", []);
+
+
+app.controller("TodoController", function ($scope) {
+    $scope.todoList = [];
+    $scope.todoCompletedTaskList=[];
+    $scope.addTask = function (newTask) {
+        var todoItem = { name: newTask, status: 'I' };
+        $scope.todoList.push(todoItem);
+    };
+
+    $scope.completeTask = function (task) {
+        task.status = 'C';
+        statusClass = "fa fa-check-circle-o";
+    };
+
+    $scope.clearCompletedTask = function () {
+
+        angular.forEach($scope.todoList, function (task) {
+            var data = task;
+            this.push(data);
+
+            $scope.todoList.splice($scope.todoList.indexOf(task), 1);
+
+        }, $scope.todoCompletedTaskList);
+    };
+});
