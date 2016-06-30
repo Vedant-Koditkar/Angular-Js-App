18071436fb8d0a9709c45fa6dbad58dda549d6dc
Vedant Koditkar <vedkoditkar@gmail.com>
2016-06-30 15:16:56 +0530

    Final commit

--- TODO_List_AngularJs_App/Scripts/ApplicationScripts/app.js
+++ TODO_List_AngularJs_App/Scripts/ApplicationScripts/app.js
@@ -5,25 +5,57 @@ var app = angular.module("TodoApp", []);
 
 app.controller("TodoController", function ($scope) {
     $scope.todoList = [];
-    $scope.todoCompletedTaskList=[];
+    $scope.todoCompletedTaskList = [];
+    $scope.showError = false;
+    $scope.taskCompletedPercentage=0;
+    $scope.incompleted=0;
+    $scope.completed=0;
+
     $scope.addTask = function (newTask) {
-        var todoItem = { name: newTask, status: 'I' };
-        $scope.todoList.push(todoItem);
+        if (newTask) {
+            var todoItem = { name: newTask, status: 'I' };
+            $scope.todoList.push(todoItem);
+            $scope.showError = false;
+        } else {
+            $scope.showError = true;
+        }
+        $scope.newTask = "";
+
+        $scope.incompleted = $scope.todoList.filter(function (task) { return task.status === 'I' }).length;
+        $scope.completed = ($scope.todoList.length - $scope.incompleted) + $scope.todoCompletedTaskList.length;
+        $scope.taskCompletedPercentage = ($scope.completed * 100 / ($scope.incompleted + $scope.completed)).toFixed(2);
+
+        return false;
     };
 
     $scope.completeTask = function (task) {
         task.status = 'C';
         statusClass = "fa fa-check-circle-o";
+
+        $scope.incompleted = $scope.todoList.filter(function (task) { return task.status === 'I' }).length;
+        $scope.completed = ($scope.todoList.length - $scope.incompleted) + $scope.todoCompletedTaskList.length;
+        $scope.taskCompletedPercentage = ($scope.completed * 100 / ($scope.incompleted + $scope.completed)).toFixed(2);
+
+        return false;
     };
 
     $scope.clearCompletedTask = function () {
 
-        angular.forEach($scope.todoList, function (task) {
-            var data = task;
-            this.push(data);
+        //angular.forEach($scope.todoList, function (task) {
+        //    if (task.status === 'C') {
+        //        this.push(task);
+        //    }
 
-            $scope.todoList.splice($scope.todoList.indexOf(task), 1);
+        //}, $scope.todoCompletedTaskList);
 
-        }, $scope.todoCompletedTaskList);
+        $scope.todoCompletedTaskList.push(...$scope.todoList.filter(function (item) {
+            return (item.status === 'C');
+        }));
+
+        $scope.todoList = $scope.todoList.filter(function (item) {
+            return (item.status === 'I');
+        });
+
+        return false;
     };
 });
